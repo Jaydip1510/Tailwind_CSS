371a0b53886f73b2dd95075cc144f8676e13e98b
Jaydip-1111 <122079821+Jaydip-1111@users.noreply.github.com>
2024-02-27 10:42:20 +0530

    tailwindfile

---
+++ tailwind-demo/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import ContectSection from './Components/ContectSection';
+// import ProductList from './Components/ProductList';
+// import LoginPage from './Components/LoginPage';
+// import ProductsView from './Components/ProductsView';
+
+function App() {
+  return (
+    <div className="App">
+     {/* <LoginPage/> */}
+     {/* <ProductsView/> */}
+     {/* <ProductList/> */}
+     <ContectSection/>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind-demo/src/Components/LoginPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function LoginPage() {
+  return (
+    <div>
+      <h1 className='bg-red-700 text-lg font-large'>welcome Tailwind css</h1>
+    </div>
+  )
+}
+
+export default LoginPage
